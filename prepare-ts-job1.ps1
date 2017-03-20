c6b2c2b380eeac4647c261e2c067e8363cdd020d
Fr3db3rt <fr3db3rt@gmail.com>
2017-03-20 15:21:53 +0100

    Create prepare-ts-job1.ps1

---
+++ prepare-ts-job1.ps1
@@ -0,0 +1,100 @@
+#@echo off
+#echo %0 PowerShell -NoProfile -ExecutionPolicy Bypass -Command -
+#more +6 %0 | PowerShell -NoProfile -ExecutionPolicy Bypass -Command -
+#exit /b
+
+# ##########################################################################################################
+# We need this SnapIn for VEEAM first,
+# and we must of course stop, if it's missing
+# (GUI part with a little help from this site:)
+# http://www.martinlehmann.de/wp/download/powershell-gui-programmierung-fur-dummies-step-by-step/
+# ##########################################################################################################
+
+If ((Get-PSSnapin -Name VeeamPSSnapin -ErrorAction SilentlyContinue) -eq $null) {Add-PSSnapin VeeamPSSnapin}
+If ((Get-PSSnapin -Name VeeamPSSnapin -ErrorAction SilentlyContinue) -eq $null) 
+
+
+# load .NET extensions
+[void] [System.Reflection.Assembly]::LoadWithPartialName("System.Drawing") 
+[void] [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms") 
+
+# create object
+$objForm = New-Object System.Windows.Forms.Form
+
+# object location
+$objForm.StartPosition = "CenterScreen"
+
+# object size
+$objForm.Size = New-Object System.Drawing.Size(300,200)
+
+# object title
+$objForm.Text = "Überschrift"
+
+# object text
+$objLabel = New-Object System.Windows.Forms.Label
+$objLabel.Location = New-Object System.Drawing.Size(10,50) 
+$objLabel.Size = New-Object System.Drawing.Size(100,20) 
+$objLabel.Text = "Hallo Welt"
+$objLabel.Name = "Hallo Welt"
+$objForm.Controls.Add($objLabel)
+
+# output object
+[void] $objForm.ShowDialog()
+
+
+{
+Add-Type -AssemblyName PresentationCore,PresentationFramework
+$ButtonType = [System.Windows.MessageBoxButton]::OK
+$MessageboxTitle = “Get-PSSnapin -Name VeeamPSSnapin failed error:”
+
+$Messageboxbody = @"
+Das Powershell SnapIn "VEEAMPSSSnapin" konnte nicht aktiviert werden.`n`n
+Ist die VEEAM Powershell installiert?`n`n
+For more info see:`n
+http://helpcenter.veeam.com/docs/backup/powershell/`n
+https://hyperv.veeam.com/blog/how-to-use-veeam-powershell-snap-in-hyper-v-backup/`n
+"@
+
+$MessageIcon = [System.Windows.MessageBoxImage]::Stop
+[System.Windows.MessageBox]::Show($Messageboxbody,$MessageboxTitle,$ButtonType,$messageicon)
+}
+
+# ##########################################################################################################
+
+# Get-VBRLocalhost | Get-VBRServer ### Does not work!
+# Get-VBRLocalhost | Get-Member
+
+$vbrservername = Get-VBRLocalhost
+# $vbrservername != $env:computername
+Write-Host $vbrservername
+Write-Host $env:computername
+
+# Thus: Workaround
+Get-VBRServer -server $env:computername
+
+#Find-VBRHvEntity -Server WIN-SRV2016Temp
+#Find-VBRHvEntity -Server $env:computername
+#this is more elegant
+Get-VBRLocalhost | Find-VBRHvEntity
+
+
+if (Get-VBRBackupRepository -Name Tagesrepository) {Get-VBRBackupRepository -Name Tagesrepository}
+else {Write-Warning "Tagesrepository fehlt!"}
+
+if (Get-VBRBackupRepository -Name Wochenrepository) {Get-VBRBackupRepository -Name wochenrepository}
+else {Write-Warning "Wochenrepository fehlt!"}
+
+
+
+Get-VBRBackupRepository -Name Wochenrepository
+Get-VBRBackupRepository -Name Tagesrepository
+$hvserver = Find-VBRHvEntity -Server $env:computername
+$repository = Get-VBRBackupRepository -Name Tagesrepository
+Add-VBRHvBackupJob -Name "2 - Tagesrepository" -Entity $hvserver -BackupRepository $repository
+$backupjob = Get-VBRJob -Name "1 - Wochenrepository"
+Find-VBRHvEntity -Server $env:computername
+Find-VBRHvEntity -Name $env:computername
+Find-VBRHvEntity -Name "$env:computername" | Add-VBRHvJobObject -Job $backupjob
+Find-VBRHvEntity -Server "$env:computername" | Add-VBRHvJobObject -Job $backupjob
+Get-VBRJob -Name "1 - Wochenrepository" | fl
+Get-VBRJob -Name "2 - Tagesrepository" | Set-VBRJobAdvancedBackupOptions -Algorithm ReverseIncremental -EnableFullBackup:$true -FullBackupScheduleKind Daily -FullBackupDays Friday
